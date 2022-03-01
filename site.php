74686b759d389d4ccaeb7708812b0e4d8c4d8f3b
Vinicius Lima <75257622+vinylima18@users.noreply.github.com>
2022-03-01 18:50:15 -0300

    Add files via upload

---
+++ site.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Document</title>
+</head>
+<body>
+ 
+
+   <form action="site.php" method="post">
+ First Num:<input type="number" name="num1"> <br>
+ OP: <input type="text" name="op"> <br>
+ Second Num:<input type="number" name="num2"> <br>
+
+
+ 
+<input type="submit">
+
+</form>  
+
+
+<?php
+
+$num1 = $_POST["num1"];
+$num2 = $_POST["num2"];
+$op = $_POST["op"];
+
+if($op== "+") {
+echo $num1 + $num2;
+} else if($op=="-") {
+ echo $num1 - $num2;
+} else if($op=="/") {
+ echo $num1 / $num2;
+} else if($op=="*") {
+ echo $num1 * $num2;
+} else {
+ echo "Invalid operator";
+}
+
+?>
+
+
+
+</body>
+</html>
